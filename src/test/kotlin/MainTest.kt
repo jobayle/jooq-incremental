b17ea744659df756c2f1063f23a410a46f5fd012
Jon Bayle <jonathan.bayle@equisign.fr>
2024-12-05 16:34:00 +0100

    Add failing test case

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,12 +3,13 @@ import gh.db01.db.tables.Record
 import gh.db02.db.tables.Service
 import gh.db02.db.tables.ServiceExtension
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 
 class MainTest {
 
     @Test
-    fun test() {
+    fun test_generate_db01() {
         assertNotNull(Domain.DOMAIN)
         assertNotNull(Domain.DOMAIN.ID)
         assertNotNull(Domain.DOMAIN.NAME)
@@ -18,7 +19,10 @@ class MainTest {
         assertNotNull(Record.RECORD.ID)
         assertNotNull(Record.RECORD.NAME)
         assertNotNull(Record.RECORD.DESCRIPTION)
+    }
 
+    @Test
+    fun test_generate_db02() {
         assertNotNull(Service.SERVICE)
         assertNotNull(Service.SERVICE.ID)
         assertNotNull(Service.SERVICE.NAME)
@@ -29,6 +33,11 @@ class MainTest {
         assertNotNull(ServiceExtension.SERVICE_EXTENSION.MIN_VALUE)
         assertNotNull(ServiceExtension.SERVICE_EXTENSION.MAX_VALUE)
         assertNotNull(ServiceExtension.SERVICE_EXTENSION.THRESHOLD)
+    }
+
+    @Test
+    fun test_always_fails() {
+        assertFalse(Domain.DOMAIN.NAME.dataType.isString)
     }
 
 }
